47b5f13b7795fc0764f166726b094a51f111aee9
Amir Toole <amir.toole@gmail.com>
2013-05-06 21:34:54 -0400

    done first week

--- Quiz/QuizViewController.m
+++ Quiz/QuizViewController.m
@@ -8,22 +8,58 @@
 
 #import "QuizViewController.h"
 
-@interface QuizViewController ()
-
-@end
-
 @implementation QuizViewController
-
-- (void)viewDidLoad
+//first method that will be called
+-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//java equivalent: Object initwithDBName(nibNameOrNil, nibBundleOrNil)
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //call the init in super class
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if(self) {
+        //create our arrays
+        questions = [[NSMutableArray alloc] init];
+        answers = [[NSMutableArray alloc] init];
+        
+        //add questions and answers to the arrays
+        [questions addObject:@"What is 7+7=?"];
+        [answers addObject:@"14"];
+        
+        [questions addObject:@"What is the capital of Vermont?"];
+        [answers addObject:@"Montpellier"];
+        
+        [questions addObject:@"From what is cognac made from?"];
+        [answers addObject:@"Grapes"];
+    }
+    
+    currentQuestionIndex = -1;
+    
+    return self;
 }
 
-- (void)didReceiveMemoryWarning
+-(IBAction)showQuestion:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    //step to the next question
+    currentQuestionIndex++;
+    if(currentQuestionIndex == [questions count]) {
+        currentQuestionIndex = 0;
+    }
+
+    //get the string for the index of the question
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+    NSLog(@"displaying question %@", question);
+
+    //display the string in the question field
+    [questionField setText:question];
+    //clear the answer field
+    [answerField setText:@"???"];
 }
+
+-(IBAction)showAnswer:(id)sender
+{
+    NSString *answer = [answers objectAtIndex:currentQuestionIndex];
+    [answerField setText:answer];
+    
+}
+
 
 @end
