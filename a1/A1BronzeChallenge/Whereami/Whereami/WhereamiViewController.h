f8996a12078f97da43eb7f3bab02cc99e3cd5391
Amir Toole <amir.toole@caseware.com>
2013-06-23 10:16:42 -0400

    using whereami

--- a1/A1BronzeChallenge/Whereami/Whereami/WhereamiViewController.h
+++ a1/A1BronzeChallenge/Whereami/Whereami/WhereamiViewController.h
@@ -1,27 +1,23 @@
 //
-//  ViewController.h
-//  A1BronzeChallenge
+//  WhereamiViewController.h
+//  Whereami
 //
-//  Created by amir on 2013-06-16.
-//  Copyright (c) 2013 amir. All rights reserved.
+//  Created by joeconway on 7/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 #import <MapKit/MapKit.h>
 
-
-@interface A1BronzeViewController : UIViewController<CLLocationManagerDelegate, MKMapViewDelegate>
+@interface WhereamiViewController : UIViewController
+    <CLLocationManagerDelegate, MKMapViewDelegate, UITextFieldDelegate>
 {
     CLLocationManager *locationManager;
     IBOutlet MKMapView *worldView;
     IBOutlet UIActivityIndicatorView *activityIndicator;
     IBOutlet UITextField *locationTitleField;
-
 }
-
 - (void)findLocation;
 - (void)foundLocation:(CLLocation *)loc;
-
-
 @end
--- a1/A1BronzeChallenge/Whereami/Whereami/WhereamiViewController.m
+++ a1/A1BronzeChallenge/Whereami/Whereami/WhereamiViewController.m
@@ -1,49 +1,45 @@
 //
-//  ViewController.m
-//  A1BronzeChallenge
+//  WhereamiViewController.m
+//  Whereami
 //
-//  Created by amir on 2013-06-16.
-//  Copyright (c) 2013 amir. All rights reserved.
+//  Created by joeconway on 7/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import "A1BronzeViewController.h"
+#import "WhereamiViewController.h"
 #import "BNRMapPoint.h"
 
-@implementation A1BronzeViewController
+@implementation WhereamiViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    
-    if (self) {
-        //create location manager object
-        locationManager = [[CLLocationManager alloc] init];
-        
-        //setting our delegate
-        [locationManager setDelegate: self];
-        
-        //we want it to be as accurate as possible regardless of how much time/power it takes
+    if(self) {
+        locationManager = [[CLLocationManager alloc] init];        
+        [locationManager setDelegate:self];        
         [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
-        
-        //tell our manager to start looking for its location immediately
-        [locationManager startUpdatingHeading];
     }
-    
     return self;
 }
-
 - (void)viewDidLoad
 {
-    [worldView setShowsUserLocation:YES];
+    [worldView setShowsUserLocation:YES];    
 }
-
+- (BOOL)textFieldShouldReturn:(UITextField *)tf
+{
+    // This method isn't implemented yet - but will be soon.
+    [self findLocation];
+    
+    [tf resignFirstResponder];
+    
+    return YES;
+}
 - (void)findLocation
 {
     [locationManager startUpdatingLocation];
     [activityIndicator startAnimating];
-//    [locationTitleField setHidden:YES];
+    [locationTitleField setHidden:YES];
 }
-
 
 - (void)foundLocation:(CLLocation *)loc
 {
@@ -51,18 +47,17 @@
     
     // Create an instance of MapPoint with the current data
     BNRMapPoint *mp = [[BNRMapPoint alloc] initWithCoordinate:coord
-                                                        title:[locationTitleField text]];
-
-    // Add it to the map view
+                                                  title:[locationTitleField text]];
+    // Add it to the map view 
     [worldView addAnnotation:mp];
-    
+        
     // Zoom the region to this location
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coord, 250, 250);
     [worldView setRegion:region animated:YES];
     
-//    [locationTitleField setText:@""];
-//    [activityIndicator stopAnimating];
-//    [locationTitleField setHidden:NO];
+    [locationTitleField setText:@""];
+    [activityIndicator stopAnimating];
+    [locationTitleField setHidden:NO];
     [locationManager stopUpdatingLocation];
 }
 
@@ -73,7 +68,7 @@
     // How many seconds ago was this new location created?
     NSTimeInterval t = [[newLocation timestamp] timeIntervalSinceNow];
     
-    // CLLocationManagers will return the last found location of the
+    // CLLocationManagers will return the last found location of the 
     // device first, you don't want that data in this case.
     // If this location was made more than 3 minutes ago, ignore it.
     if (t < -180) {
@@ -90,7 +85,6 @@
     NSLog(@"Could not find location: %@", error);
 }
 
-
 - (void)mapView:(MKMapView *)mv didUpdateUserLocation:(MKUserLocation *)u
 {
     CLLocationCoordinate2D loc = [u coordinate];
@@ -98,12 +92,9 @@
     [worldView setRegion:region animated:YES];
 }
 
-
-//we have to manually dealloc this obj
 - (void)dealloc
 {
-    //tell location manager to stop sending us messages
+    // Tell the location manager to stop sending us messages
     [locationManager setDelegate:nil];
 }
-
 @end
