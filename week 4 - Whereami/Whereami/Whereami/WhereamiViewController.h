c6cff2152723ac3ca59997e96c4b1cd9d3b496d3
amir toole <amir.toole@gmail.com>
2013-06-24 19:57:16 -0400

    week 4 init

---
+++ week 4 - Whereami/Whereami/Whereami/WhereamiViewController.h
@@ -0,0 +1,22 @@
+//
+//  WhereamiViewController.h
+//  Whereami
+//
+//  Created by joeconway on 7/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+
+@interface WhereamiViewController : UIViewController <CLLocationManagerDelegate, MKMapViewDelegate>
+{
+    CLLocationManager *locationManager;
+    
+    IBOutlet MKMapView *worldView;
+    IBOutlet UIActivityIndicatorView *activityIndicator;
+    IBOutlet UITextField *locationTitleField;
+}
+
+@end
