2305edb39eec9d1a151011f11ec99486f061ed04
amir toole <amir.toole@gmail.com>
2013-07-15 21:14:43 -0400

    done heavy rotation

---
+++ week 5 - heavy rotation/HeavyRotation/HeavyRotation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeavyRotation
+//
+//  Created by iadmin on 13-07-15.
+//  Copyright (c) 2013 amir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotationAppDelegate class]));
+    }
+}
