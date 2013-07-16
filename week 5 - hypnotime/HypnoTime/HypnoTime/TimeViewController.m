b89f363cf47d5837b6fb173c5ff923f75497304b
amir toole <amir.toole@gmail.com>
2013-07-15 20:01:30 -0400

    time updating every second

---
+++ week 5 - hypnotime/HypnoTime/HypnoTime/TimeViewController.m
@@ -0,0 +1,58 @@
+//
+//  TimeViewController.m
+//  HypnoTime
+//
+//  Created by iadmin on 13-07-15.
+//  Copyright (c) 2013 amir. All rights reserved.
+//
+
+#import "TimeViewController.h"
+
+@interface TimeViewController ()
+
+@end
+
+@implementation TimeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    NSRunLoop *runloop = [NSRunLoop currentRunLoop];
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self  selector:@selector(updateTime) userInfo:nil repeats:YES];
+    [runloop addTimer:timer forMode:NSRunLoopCommonModes];
+    [runloop addTimer:timer forMode:UITrackingRunLoopMode];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showCurrentTime:(id)sender
+{
+    [self updateTime];
+}
+
+- (void)updateTime
+{
+    NSDate *now = [NSDate date];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setTimeStyle:NSDateFormatterMediumStyle];
+    
+    [timeLabel setText:[formatter stringFromDate:now]];
+
+}
+
+@end
