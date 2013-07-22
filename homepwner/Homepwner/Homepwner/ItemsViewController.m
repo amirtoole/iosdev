5466f1caefd5d6b1637aab1ae6b25028a64f742e
amir toole <amir.toole@gmail.com>
2013-07-22 19:22:57 -0400

    finished chapter 8

--- homepwner/Homepwner/Homepwner/ItemsViewController.m
+++ homepwner/Homepwner/Homepwner/ItemsViewController.m
@@ -7,15 +7,19 @@
 //
 
 #import "ItemsViewController.h"
+#import "BNRItemStore.h"
+#import "BNRItem.h"
 
 @implementation ItemsViewController
 
 - (id)init
-{
+        {
     //call the super class's designated init
     self = [super initWithStyle:UITableViewStyleGrouped];
     if (self) {
-        
+        for (int i = 0; i < 5; i++) {
+            [[BNRItemStore sharedStore] createItem];
+        }
     }
     return self;
 }
@@ -23,6 +27,27 @@
 - (id)initWithStyle:(UITableViewStyle)style
 {
     return [self init];
+}
+            
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //create an instance of UITableViewCell w/ default appearance
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                   reuseIdentifier:@"UITableViewCell"];
+    
+    //set the text on the cell w/ the description of the item that is at the nth index of items,
+    //where n = row this cell will appear on the tableview
+    BNRItem *p = [[[BNRItemStore sharedStore] allItems]
+                  objectAtIndex:[indexPath row]];
+    
+    [[cell textLabel] setText:[p description]];
+    
+    return cell;
 }
 
 @end
