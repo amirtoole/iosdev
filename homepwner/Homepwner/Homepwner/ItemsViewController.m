251cc4d14434cfee0680ad3e6b9457bf5e83798a
amir toole <amir.toole@gmail.com>
2013-07-22 20:14:14 -0400

    added header

--- homepwner/Homepwner/Homepwner/ItemsViewController.m
+++ homepwner/Homepwner/Homepwner/ItemsViewController.m
@@ -17,7 +17,7 @@
     //call the super class's designated init
     self = [super initWithStyle:UITableViewStyleGrouped];
     if (self) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 500; i++) {
             [[BNRItemStore sharedStore] createItem];
         }
     }
@@ -36,9 +36,15 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    //create an instance of UITableViewCell w/ default appearance
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                                   reuseIdentifier:@"UITableViewCell"];
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
+    
+    //if there is no reusable cell of this type, create a new one
+    if (!cell) {
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleDefault
+                reuseIdentifier:@"UITableViewCell"];
+    }
     
     //set the text on the cell w/ the description of the item that is at the nth index of items,
     //where n = row this cell will appear on the tableview
@@ -48,6 +54,38 @@
     [[cell textLabel] setText:[p description]];
     
     return cell;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    return [self headerView];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    //the height of the header should be determined from the height of the view in the xib
+    return [[self headerView] bounds].size.height;
+}
+
+- (UIView *)headerView
+{
+    //if we haven't loaded the headerView yet ...
+    if (!headerView) {
+        //load the HeaderView.xib
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
+    }
+    
+    return headerView;
+}
+
+- (void)addNewItem:(id)sender
+{
+        
+}
+
+- (void)toggleEditingMode:(id)sender
+{
+    
 }
 
 @end
