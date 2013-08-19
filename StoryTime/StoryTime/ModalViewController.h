946541a2ac6736722247a6671a6969fce7a4301f
amir toole <amir.toole@gmail.com>
2013-08-19 19:27:02 -0400

    done storyboard intro

---
+++ StoryTime/StoryTime/ModalViewController.h
@@ -0,0 +1,15 @@
+//
+//  ModalViewController.h
+//  StoryTime
+//
+//  Created by iadmin on 13-08-19.
+//  Copyright (c) 2013 amir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+
+- (IBAction)dismiss:(id)sender;
+
+@end
---
+++ StoryTime/StoryTime/ModalViewController.m
@@ -0,0 +1,43 @@
+//
+//  ModalViewController.m
+//  StoryTime
+//
+//  Created by iadmin on 13-08-19.
+//  Copyright (c) 2013 amir. All rights reserved.
+//
+
+#import "ModalViewController.h"
+
+@interface ModalViewController ()
+
+@end
+
+@implementation ModalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)dismiss:(id)sender
+{
+    [[self presentingViewController] dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
